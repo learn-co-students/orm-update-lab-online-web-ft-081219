592a5603ae58146ce2d0c2bd6859ba23468558ee
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-24 15:52:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -51,11 +51,8 @@ class Student
     stud 
   end
   
-  def self.new_from_db
-    sql = <<-SQL
-      
-    SQL
-    DB[:conn].execute(sql)
+  def self.new_from_db(row)
+    self.new(row[0], row[1], row[2])
   end
   
   def self.find_by_name(name)
